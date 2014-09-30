2c127137fc3eefc2d98102981dd9b57ae64d27df
gaofeihang <gaofeihang@163.com>
2014-09-30 19:59:51 +0800

    first commit

---
+++ s025-web/src/main/java/org/s025/web/controller/IndexController.java
@@ -0,0 +1,31 @@
+package org.s025.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author gaofeihang
+ * @since Sep 30, 2014
+ *
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public String handle(HttpServletRequest request, ModelMap model) {
+        return "redirect:/index";
+    }
+    
+    @RequestMapping("/index")
+    public void hello(HttpServletRequest request, ModelMap model, String name) {
+        if (name == null) {
+            name = "World";
+        }
+        
+        model.put("name", name);
+    }
+
+}
