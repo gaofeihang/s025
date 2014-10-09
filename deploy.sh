c27bf5de547a4b62270589b42a80c94d4b7ac377
gaofeihang <gaofeihang@163.com>
2014-10-09 14:33:20 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/workspace/git/s025/
 git pull
 mvn clean package -Dmaven.test.skip
 mv /home/workspace/git/s025/s025-web/target/s025-web-0.0.1-SNAPSHOT.war /home/server/tomcat/apache-tomcat-8.0.12/webapps/ROOT.war
