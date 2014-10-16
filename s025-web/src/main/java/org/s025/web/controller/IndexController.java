71ace2bc0b6774782d0ae7d814f4060ea35b56c4
gaofeihang <gaofeihang@163.com>
2014-10-16 16:30:36 +0800

    百度地图调整

--- s025-web/src/main/java/org/s025/web/controller/IndexController.java
+++ s025-web/src/main/java/org/s025/web/controller/IndexController.java
@@ -22,13 +22,17 @@ public class IndexController {
     }
     
     @RequestMapping("/index")
-    public void hello(HttpServletRequest request, ModelMap model, String name) {
+    public void index(HttpServletRequest request, ModelMap model, String name) {
         if (name == null) {
             name = "World";
         }
         
         model.put("name", name);
         model.put("date", new Date());
+    }
+    
+    @RequestMapping("/map")
+    public void map(HttpServletRequest request, ModelMap model) {
     }
 
 }
